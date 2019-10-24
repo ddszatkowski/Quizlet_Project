b47e43e8f0457ac1003ac30743ece6c3aac426eb
David Szatkowski <david@a2zware.com>
2019-10-23 23:58:18 -0500

    Added Turrets and Button Functionality
    Added two turrets that track the mouse. The turrets change to the appropriate sprite when a button is selected.

--- Quizlet Study/Scripts/Game.gd
+++ Quizlet Study/Scripts/Game.gd
@@ -9,11 +9,14 @@ onready var global = get_node("/root/global")
 onready var bar = $TextureProgress
 # List of spawned enemies
 var enemies = []
+var group
 # All selected card sets, merged
 var card_dict
-
+var Mouse_Position
 #health
 var health
+var color = "red"
+
 
 func _ready():
 	# Spawn an enemy per card in set
@@ -23,6 +26,13 @@ func _ready():
 	var player_max_health = 100
 	bar.max_value = player_max_health
 	bar.value = player_max_health
+	
+	group = ButtonGroup.new()
+	
+	$RedButton.set_button_group(group)
+	$BlueButton.set_button_group(group)
+	$GreenButton.set_button_group(group)
+	$PurpleButton.set_button_group(group)
 	
 # Declare new enemy, call initialization and add to this node as child
 func add_enemy(question):
@@ -45,3 +55,19 @@ func take_damage(count):
 
 	bar.value = health
 	
+
+func _on_RedButton_toggled(button_pressed):
+	print("Red")
+	color = "red"
+	
+func _on_BlueButton_toggled(button_pressed):
+	print("Blue")
+	color = "blue"
+	
+func _on_GreenButton_toggled(button_pressed):
+	print("Green")
+	color = "green"
+	
+func _on_PurpleButton_toggled(button_pressed):
+	print("Purple")
+	color = "purple"
---
+++ Quizlet Study/Scripts/Turret.gd
@@ -0,0 +1,37 @@
+extends Sprite
+
+var shoot
+var Mouse_Position
+export (PackedScene) var laser
+onready var Laser_container = get_node("Laser_container")
+var red_tex = load("res://Sprites/red_turret.png")
+var blue_tex = load("res://Sprites/blue_turret.png")
+var green_tex = load("res://Sprites/green_turret.png")
+var purple_tex = load("res://Sprites/purple_turret.png")
+
+
+func _ready():
+	set_process_input(true)
+	texture = red_tex
+
+func _shoot():
+	look_at(get_global_mouse_position())
+	var pew = laser.instance()
+	Laser_container.add_child(pew)
+	
+	pew.shoot(get_global_mouse_position(), global_position)
+	
+func _process(delta):
+	Mouse_Position = get_local_mouse_position()
+	rotation += Mouse_Position.angle() +.8
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if get_parent().color == "red":
+			texture = red_tex
+		if get_parent().color == "blue":
+			texture = blue_tex
+		if get_parent().color == "green":
+			texture = green_tex
+		if get_parent().color == "purple":
+			texture = purple_tex
