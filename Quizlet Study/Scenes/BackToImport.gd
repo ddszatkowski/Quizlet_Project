b2c9a7c0b4460ded4b890795f9f1bd498b14e457
Joelle Frye <jfhn6@mst.edu>
2019-11-26 12:28:44 -0600

    added new scene for the making of new cards

--- Quizlet Study/Scenes/BackToImport.gd
+++ Quizlet Study/Scenes/BackToImport.gd
@@ -3,6 +3,4 @@ var optionButton = OptionButton
 onready var global = get_node("/root/global")
 
 func _on_New_pressed():
-	get_tree().change_scene("res://Scenes/MainMenu.tscn")
-
-
+	get_tree().change_scene("res://Scenes/ImportMenu.tscn")
